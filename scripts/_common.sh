b5ff58ac2fad4adab7e1e62374eab8fcb354866b
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-10-24 22:03:12 +0200

    first commit

---
+++ scripts/_common.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES AND CUSTOM HELPERS
+#=================================================
+
+nodejs_version="20"
+mongo_version="6.0"
